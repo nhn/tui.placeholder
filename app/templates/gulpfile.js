b9cf1d97002d9115861424b92bf65a6dcd7be340
NHNEnt <nhnent@NHNEnts-MacBook-Pro.local>
2016-03-15 20:16:52 +0900

    # ?

--- app/templates/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var connect = require('gulp-connect');
-
-gulp.task('connect', function() {
-    connect.server(<% if (nolivereload) { %>{
-        livereload: false
-    }<% } %>);
-});
-
