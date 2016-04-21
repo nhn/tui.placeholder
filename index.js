859ade7493fe3793971533c69d1fc083856e1327
NHNEnt <nhnent@NHNEnts-MacBook-Pro.local>
2016-04-21 17:47:14 +0900

    recover

--- index.js
+++ index.js
@@ -2,6 +2,4 @@
 
 tui.util.defineNamespace('tui.component.placeholder', require('./src/placeholder.js'));
 
-setTimeout(function() {
-    tui.component.placeholder();
-}, 0);
+tui.component.placeholder();
