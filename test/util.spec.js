831e7c791db4152e3a1f4e93b35b141f1ca73074
dongwoo <dongwoo.kim@nhnent.com>
2016-08-08 17:19:49 +0900

    test: update spec title

--- test/util.spec.js
+++ test/util.spec.js
@@ -10,29 +10,31 @@ var util = require('../src/util');
 
 describe('util.js', function() {
     describe('bindEvent', function() {
-        it('should bind event handler to target element', function() {
+        it('should bind event handler to a target element', function() {
             var callback = jasmine.createSpy('callback');
             var input = $('<input>')[0];
 
             jasmine.getFixtures().set(input);
             util.bindEvent(input, 'click', callback);
+
             simulant.fire(input, 'click');
 
             expect(callback).toHaveBeenCalled();
         });
 
-        it('should set callback function as a prop to target element', function() {
+        it('should set callback function as a prop to a target element', function() {
             var input = $('<input>')[0];
             var propName = util._callbackPropName('click');
             var callback = function() {};
 
             util.bindEvent(input, 'click', callback);
+
             expect(input[propName]).toBe(callback);
         });
     });
 
     describe('unbindEvent', function() {
-        it('should unbind event handler from target element', function() {
+        it('should unbind event handler from a target element', function() {
             var callback = jasmine.createSpy('callback');
             var input = $('<input>')[0];
 
@@ -45,13 +47,14 @@ describe('util.js', function() {
             expect(callback).not.toHaveBeenCalled();
         });
 
-        it('should set callback function as a prop to target element', function() {
+        it('should delete a callback prop from a target element', function() {
             var input = $('<input>')[0];
             var propName = util._callbackPropName('click');
             var callback = function() {};
 
             util.bindEvent(input, 'click', callback);
             util.unbindEvent(input, 'click');
+
             expect(input[propName]).toBeUndefined();
         });
     });
